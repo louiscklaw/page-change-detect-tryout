3554ac12e229d72c625bcc7ca93bca09b68a9f28
louiscklaw <louiscklaw@gmail.com>
2020-07-12 18:14:29 +0800

    init commit,

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is a personal page change detector
+https://www.mi.com/hk/redmi-note-9-pro/
---
+++ build_main.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd check-xiao-mi-9-pro-price
+  scripts/build.sh
+
+cd ..
