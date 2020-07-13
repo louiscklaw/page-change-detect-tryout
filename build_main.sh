59d89cf0142df272a8835f691669e22ba49a5201
louiscklaw <louiscklaw@gmail.com>
2020-07-14 01:48:50 +0800

    upadte for execute monitor on taobao,

--- build_main.sh
+++ build_main.sh
@@ -2,7 +2,13 @@
 
 set -ex
 
+# monitor xiaomi on offical page for lower price
 cd check-xiao-mi-9-pro-price
   scripts/build.sh
 
+cd ..
+
+# monitor GTX1050 ti on taobao
+cd taobao-item-monitor
+  scripts/build.sh
 cd ..
