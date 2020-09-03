47e6dce75075f3c0e093d46416a73b09bbb5b575
louiscklaw <louiscklaw@gmail.com>
2020-09-03 17:19:56 +0800

    update,

--- jobsdb-qa-monitor-tryout/test/test_hubdb.js
+++ jobsdb-qa-monitor-tryout/test/test_hubdb.js
@@ -25,27 +25,37 @@ function test_add(){
 
 function test_list(){
   return db.list((err, a)=>{
-    console.log(JSON.stringify(a, null, 2))
-    return a.map(json_file => {
-      try {
-        db.update(json_file.path, {another:'helloworld'},(err, result, id)=>{
-          if (err) throw err
-          console.log('update done')
-          console.log('result',result)
-        })
+    // console.log(JSON.stringify(a, null, 2))
+    const paths = a.map(x => x.path)
+    const first_path = paths[0]
 
-      } catch (error) {
-        console.error(`error during processing file ${json_file.path}`)
-      }
+    db.update(first_path,{hello:'first_path'}, (err, result, id)=>{
+      console.log('result',result)
     })
+
   })
 
 }
 
+function test_get(cb){
+  db.get("another_helloworld_db.json",(err, contents)=>{
+    console.log('get,', contents)
+  })
+  return cb()
+}
+
+function test_update(){
+  db.update("another_helloworld_db.json",{hello:'world'},(err, result, id)=>{
+    console.log('result,', result)
+    console.log('id,',id)
+  })
+
+}
 
 function test(){
  console.log(`testing ${path.basename(__filename)}`)
  test_add()
+ test_list()
 }
 
 module.exports={
