ce3f9fc1ed9767a2663015d2f709a8498c4a3559
louiscklaw <louiscklaw@gmail.com>
2020-09-05 20:01:25 +0800

    update and test,

--- puppeteer-tryout/index.js
+++ puppeteer-tryout/index.js
@@ -3,4 +3,21 @@ const assert = require('assert');
 const puppeteer = require('puppeteer');
 
 
+const tidyParagraph = (para_in) => {
+  return para_in.replace(/ /g,'').replace(/\n/g,'')
+}
+
+
+async function xiaomiPriceChangeCheck() {
+  const browser = await puppeteer.launch({
+    ignoreHTTPSErrors: true,
+    defaultViewport: {
+      width: 1920,
+      height:1080*10
+    }
+  });
+}
+
+xiaomiPriceChangeCheck();
+
 console.log('helloworld')
