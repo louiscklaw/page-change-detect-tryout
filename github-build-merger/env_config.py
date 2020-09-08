be4870a602089094cbab3bbda4880a0ac828ff13
louiscklaw <louiscklaw@gmail.com>
2020-09-08 17:29:19 +0800

    update test merger,

--- github-build-merger/env_config.py
+++ github-build-merger/env_config.py
@@ -10,6 +10,6 @@ GITHUB_REF=os.environ['GITHUB_REF']
 TRIGGERING_BRANCH=GITHUB_REF.replace('refs/heads/','')
 
 GITHUB_REPO = os.environ['GITHUB_REPOSITORY']
-GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+GITHUB_TOKEN = os.environ['MY_GITHUB_TOKEN']
 
 PUSH_URI="https://{}@github.com/{}".format(GITHUB_TOKEN, GITHUB_REPO)
